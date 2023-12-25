646d1ad61d553ff31f5b5163d1c449fd654db053
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2023-12-25 23:08:41 +0500

    commit rddle

--- Functions/BattleSystem/source.cpp
+++ Functions/BattleSystem/source.cpp
@@ -2,8 +2,8 @@
 #include <string>
 using namespace std;
 
-int quest1 {
-
+int quest1 { 
+string rddlans;
 cout<<"LEVEL 1: Explore the Forest"<<endl;
 
 cout<<"PROMPT: ....Travelling across the forest..... "<<endl;
@@ -12,11 +12,12 @@ cout<<"PROMPT: You have reach Grovetopia Falls "<<endl;
 cout<<"Wait! What's that dark corner right there?...."<<endl;
 cout<<"A Cave?? I wonder."<<endl;
 
-cout<<"PROMPT: TO ENTER THE CAVE, YOU MUST SOLVE THE RIDDLE (3 Choices)";
- 
+cout<<"PROMPT: TO ENTER THE CAVE, YOU MUST SOLVE THE RIDDLE (3 Attempts)"<<endl;
+
+cout<<"What is brown and sticky?"<<endl;
+cin
 
 
-cout<<""
 
 
 

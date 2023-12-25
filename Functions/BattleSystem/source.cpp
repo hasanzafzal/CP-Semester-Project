b8b454d0547df2427ca113cf4d2f52ccf04ea2c5
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2023-12-25 22:59:36 +0500

    riddle

--- Functions/BattleSystem/source.cpp
+++ Functions/BattleSystem/source.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int quest1 {
 
-cout<<"LEVEL 1: Explore the Forest";
+cout<<"LEVEL 1: Explore the Forest"<<endl;
+
+cout<<"PROMPT: ....Travelling across the forest..... "<<endl;
+cout<<"PROMPT: You have reach Grovetopia Falls "<<endl;
+
+cout<<"Wait! What's that dark corner right there?...."<<endl;
+cout<<"A Cave?? I wonder."<<endl;
+
+cout<<"PROMPT: TO ENTER THE CAVE, YOU MUST SOLVE THE RIDDLE (3 Choices)";
+ 
+
+
+cout<<""
+
 
 
 
